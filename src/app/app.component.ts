d339d5a69dc96fa8ecda09c8b7272c4158e0f603
Angela Ordonez <angelitaooo@me.com>
2017-07-02 21:16:15 -0500

    solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  @ViewChild('titleElement') h1ElementInTemplate;
+  @ViewChild('heroElement') heroElementInTemplate;
+
+  walk = 500; // 100px
+
+  shadow(e) {
+    const height = this.heroElementInTemplate.nativeElement.offsetHeight;
+    const width = this.heroElementInTemplate.nativeElement.offsetWidth;
+    let x = e.offsetX;
+    let y = e.offsetY;
+
+    if (this.heroElementInTemplate.nativeElement !== e.target) {
+      x = x + e.target.offsetLeft;
+      y = y + e.target.offsetTop;
+    }
+
+    const xWalk = Math.round((x / width * this.walk) - (this.walk / 2));
+    const yWalk = Math.round((y / height * this.walk) - (this.walk / 2));
+
+    const newStyles = `
+      ${xWalk}px ${yWalk}px 0 rgba(255,0,255,0.7),
+      ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,255,0.7),
+      ${yWalk}px ${xWalk * -1}px 0 rgba(0,255,0,0.7),
+      ${yWalk * -1}px ${xWalk}px 0 rgba(0,0,255,0.7)
+    `;
+
+    this.h1ElementInTemplate.nativeElement.style.textShadow = newStyles;
+  }
 }
